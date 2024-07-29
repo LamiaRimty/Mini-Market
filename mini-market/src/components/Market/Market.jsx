259a054b5b3aa28e4c9c439dd3d1066d3b8d3f0b
Lamia Jabin Rimty <rimtycse@gmail.com>
2024-07-29 20:56:38 +0200

    Marketlist rendering

--- mini-market/src/components/Market/Market.jsx
+++ mini-market/src/components/Market/Market.jsx
@@ -1,1 +1,20 @@
-export default function MarketList() {}
+const products = [
+  { title: "Orange🍊", isFruits: true, id: 1 },
+  { title: "Aubergin🍆", isFruits: false, id: 2 },
+  { title: "Carrot🥕", isFruits: false, id: 3 },
+];
+
+export default function MarketList() {
+  const listItems = products.map((product) => (
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruits ? "orange" : "lightgreen",
+      }}
+    >
+      {product.title}
+    </li>
+  ));
+
+  return <li>{listItems}</li>;
+}
