0d92ea688d656e75da89d3819bc42175ba4874d7
Lamia Jabin Rimty <rimtycse@gmail.com>
2024-07-29 20:33:31 +0200

    Setup the local environment

---
+++ mini-market/src/App.js
@@ -0,0 +1,16 @@
+
+import MarketList from "./components/Market/Market";
+
+function App() {
+
+  return (
+  <>
+  <h1>🛍️Mini Market🛍️ </h1>
+    <MarketList/>
+   
+    </>
+  );
+}
+
+
+export default App;
