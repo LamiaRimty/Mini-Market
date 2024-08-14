e3dd2b9be4dc22997abad62d5b517c27729b9fe7
Lamia Jabin Rimty <rimtycse@gmail.com>
2024-08-14 18:32:06 +0200

    added product category component

--- mini-market/src/components/Market/Market.jsx
+++ mini-market/src/components/Market/Market.jsx
@@ -31,10 +31,6 @@ function SearchBar() {
   );
 }
 
-function ProductTable() {
-  return <></>;
-}
-
 function ProductRow({ product }) {
   return (
     <tr>
@@ -44,11 +40,42 @@ function ProductRow({ product }) {
   );
 }
 
-function productCategoryRow({ category }) {
+function ProductCategoryRow({ category }) {
   return (
-    <tr>
+    <tr colSpan="2">
       <th>{category}</th>
     </tr>
+  );
+}
+
+function ProductTable({ products }) {
+  const rows = [];
+  let lastCategory = null;
+
+  products.forEach((product) => {
+    if (product.category !== lastCategory) {
+      rows.push(
+        <ProductCategoryRow
+          category={product.category}
+          key={product.category}
+        />
+      );
+    }
+    rows.push(<ProductRow product={product} key={product.name} />);
+    lastCategory = product.category;
+  });
+
+  return (
+    <table>
+      <thread>
+        <tr>
+          <th>Name</th>
+
+          <th>Price</th>
+        </tr>
+      </thread>
+      <tbody>{rows}</tbody>
+    </table>
   );
 }
 
