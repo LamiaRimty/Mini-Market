3f845dec8a62222b1c19ddad3ea744b890388412
Lamia Jabin Rimty <rimtycse@gmail.com>
2024-08-14 18:39:17 +0200

    highlighted the cheap products

--- mini-market/src/components/Market/Market.jsx
+++ mini-market/src/components/Market/Market.jsx
@@ -32,9 +32,15 @@ function SearchBar() {
 }
 
 function ProductRow({ product }) {
+  const name = product.stocked ? (
+    product.name
+  ) : (
+    <span style={{ color: "orange" }}>{product.name}</span>
+  );
+
   return (
     <tr>
-      <td>{product.name}</td>
+      <td>{name}</td>
       <td>{product.price}</td>
     </tr>
   );
